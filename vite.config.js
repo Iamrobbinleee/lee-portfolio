4be48da455fc7a83eaef15986277c6e049007a26
Iamrobbinleee <leerobinroxas8@gmail.com>
2025-03-03 23:30:07 +0800

    modified vite.config.js to display image and assets.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/lee-portfolio/" : "/",
+  base: import.meta.env.MODE === "production" ? "/lee-portfolio/" : "/",
   plugins: [vue()],
 });
